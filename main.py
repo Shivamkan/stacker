31b893fe4cc6f001faa234669a2f7c020f712dda
shivam <shivamkantprasad@gmail.com>
2021-06-03 16:19:36 +0530

    Now you can play infinty there is not hight limit. Game resets if the block is small then 0.

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import pygame
 
+pygame.init()
+
+screenY = int(650)
+screenX = int((screenY / 16) * 9)
+colors = 10
+auto = 2
+sound = pygame.mixer.Sound("perfect.mp3")
+#Do Not Change Anything after this ponit
+screen = pygame.display.set_mode((screenX, screenY))
+down = 0
+
 def hls(h, s, l):
 	h = h / 255
 	s = s / 255
@@ -34,16 +45,6 @@ def hls(h, s, l):
 	return (round(r * 255), round(g * 255), round(b * 255))
 
 
-pygame.init()
-
-screenY = int(650)
-screenX = int((screenY / 16) * 9)
-colors = 10
-auto = 5
-sound = pygame.mixer.Sound("perfect.mp3")
-
-screen = pygame.display.set_mode((screenX, screenY))
-
 class main:
 	def __init__(self):
 		self.blockhight = int(screenY / 30)
@@ -57,19 +58,14 @@ class main:
 		self.dir = 1
 
 	def draw(self, screen):
-		if self.lastsuf:
+		if self.lastsuf and not down:
 			screen.blit(self.lastsuf,(0,0))
+		elif down:
+			screen.blit(self.lastsuf,(0,self.blockhight))
 		pygame.draw.rect(screen, hls(self.hue, 255, 255 / 2), (self.x, self.y, self.width, self.blockhight))
 		if self.place == 1:
 			self.lastsuf = screen.copy()
-			self.y -= self.blockhight
-			self.place = 0
-			self.hue += 255/colors
-			self.last = self.x
-			self.x = 0
-
-		if self.hue > 254:
-			self.hue = 0
+			self.reset()
 
 	def move(self):
 		if self.dir == 1:
@@ -105,6 +101,30 @@ class main:
 			self.x = self.last
 		self.dir = 1
 
+	def reset(self):
+		global down
+		if self.y > (screenY/3):
+			self.y -= self.blockhight
+		else:
+			down = 1
+		self.place = 0
+		self.hue += 255 / colors
+		self.last = self.x
+		self.x = 0
+		if self.hue > 254:
+			self.hue = 0
+
+def resetgame(main):
+	main.blockhight = int(screenY / 30)
+	main.hue = 0
+	main.y = screenY - main.blockhight
+	main.x = (screenX / 12) * 1
+	main.width = (screenX / 12) * 10
+	main.place = 1
+	main.lastsuf = 0
+	main.last = main.x
+	main.dir = 1
+
 main = main()
 clock = pygame.time.Clock()
 while True:
@@ -114,3 +134,5 @@ while True:
 	main.move()
 	main.handle_input()
 	pygame.display.flip()
+	if main.width <= 0:
+		resetgame(main)
