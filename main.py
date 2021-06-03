b70bedc49d46b0869959e6a828de6bf9a6d835a9
shivam <shivamkantprasad@gmail.com>
2021-06-03 19:02:15 +0530

    added score counter.

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ screenX = int((screenY / 16) * 9)
 colors = 50
 auto = 2
 sound = pygame.mixer.Sound("perfect.mp3")
-#Do Not Change Anything after this ponit
+font = pygame.font.Font(r"font.ttf", int(screenX/6), italic=True)
+# Do Not Change Anything after this point ==========================================
 screen = pygame.display.set_mode((screenX, screenY))
 down = 0
+end = 0
+
 
 def hls(h, s, l):
 	h = h / 255
@@ -49,6 +52,7 @@ class main:
 	def __init__(self):
 		self.blockhight = int(screenY / 30)
 		self.hue = 0
+		self.scorecolor = 0
 		self.y = screenY - self.blockhight
 		self.x = (screenX / 12) * 1
 		self.width = (screenX / 12) * 10
@@ -60,13 +64,21 @@ class main:
 
 	def draw(self, screen):
 		if self.lastsuf and not down:
-			screen.blit(self.lastsuf,(0,0))
+			screen.blit(self.lastsuf, (0, 0))
 		elif down and self.lastsuf:
-			screen.blit(self.lastsuf,(0,self.blockhight))
+			screen.blit(self.lastsuf, (0, self.blockhight))
 		pygame.draw.rect(screen, hls(self.hue, 255, 255 / 2), (self.x, self.y, self.width, self.blockhight))
 		if self.place == 1:
 			self.lastsuf = screen.copy()
 			self.reset()
+		score = font.render("score:", 0, hls(self.scorecolor, 255, 255 / 2))
+		screen.blit(score, (0, -10))
+		scoresize = score.get_size()
+		score = font.render(str(self.score), 0, hls(self.scorecolor, 255, 255 / 2))
+		screen.blit(score, (scoresize[0]/2-score.get_size()[0]/2, scoresize[1]-20))
+		self.scorecolor += 0.5
+		if self.scorecolor > 254:
+			self.scorecolor = 0
 
 	def move(self):
 		if self.dir == 1:
@@ -79,15 +91,20 @@ class main:
 			self.dir = 1
 
 	def handle_input(self):
+		global end
 		events = pygame.event.get()
 		for event in events:
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				quit()
-			if event.type==pygame.KEYDOWN:
+			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_SPACE:
+					if self.width <= 0:
+						end = 1
 					self.placeb()
-			if event.type == pygame.MOUSEBUTTONDOWN:# and event.button == 1:
+			if event.type == pygame.MOUSEBUTTONDOWN:  # and event.button == 1:
+				if self.width <= 0:
+					end = 1
 				self.placeb()
 
 	def placeb(self):
@@ -97,14 +114,18 @@ class main:
 			sound.play()
 		elif self.x > self.last:
 			self.width -= self.x - self.last
+			if self.width > 0:
+				self.score += 1
 		else:
 			self.width -= self.last - self.x
 			self.x = self.last
+			if self.width > 0:
+				self.score += 1
 		self.dir = 1
 
 	def reset(self):
 		global down
-		if self.y > (screenY/3):
+		if self.y > (screenY / 3):
 			self.y -= self.blockhight
 		else:
 			down = 1
@@ -115,8 +136,9 @@ class main:
 		if self.hue > 254:
 			self.hue = 0
 
+
 def resetgame(main):
-	global down
+	global down, end
 	main.blockhight = int(screenY / 30)
 	main.hue = 0
 	main.y = screenY - main.blockhight
@@ -128,6 +150,8 @@ def resetgame(main):
 	main.dir = 1
 	main.score = 0
 	down = 0
+	end = 0
+
 
 main = main()
 clock = pygame.time.Clock()
@@ -138,5 +162,5 @@ while True:
 	main.move()
 	main.handle_input()
 	pygame.display.flip()
-	if main.width <= 0:
+	if end:
 		resetgame(main)
